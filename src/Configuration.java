3a519eaa154e86a461905722e89a8d0a93cfa76d
SJacobs <siminjacobs@gmail.com>
2016-08-16 11:27:49 +0200

    init

---
+++ src/Configuration.java
@@ -0,0 +1,16 @@
+public class Configuration {
+
+	public static ClientMode clientMode;
+
+	public static int clientSize = 0, clientWidth = 765, clientHeight = 503,
+			REGULAR_WIDTH = 765, REGULAR_HEIGHT = 503, RESIZABLE_WIDTH = 800,
+			RESIZABLE_HEIGHT = 600;
+
+	public static boolean runClicked;
+
+	public static boolean debug = false;
+	public static boolean hitmarks554 = false;
+	public static boolean runEnergy = false;
+	public static boolean newMenu = false;
+
+}
